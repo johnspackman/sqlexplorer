6d1cfdf463e5df7ee60b78a42c62d13fcf54725f
cpplus <>
2006-10-04 14:18:23 +0000

    Initial version to display privilege of function.

---
+++ sqlexplorer_db2/src/net/sourceforge/sqlexplorer/db2/tabs/FunctionPrivilegeTab.java
@@ -0,0 +1,45 @@
+package net.sourceforge.sqlexplorer.db2.tabs;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import net.sourceforge.sqlexplorer.Messages;
+import net.sourceforge.sqlexplorer.dataset.DataSet;
+import net.sourceforge.sqlexplorer.dataset.DataSetRow;
+import net.sourceforge.sqlexplorer.dbdetail.tab.AbstractDataSetTab;
+
+public class FunctionPrivilegeTab extends AbstractDataSetTab {
+	private static final Log _logger = LogFactory.getLog(ProcedurePrivilegeTab.class);
+	
+    public String getLabelText() {
+    	return Messages.getString("DatabaseDetailView.Tab.Priviliges");
+    }
+
+    public DataSet getDataSet() throws Exception {       
+        
+    	String schemaName = getNode().getSchemaOrCatalogName();
+        String nodeName = getNode().toString();
+        _logger.debug("nodeName is: " + nodeName);
+        String specificName = this.getSpecificName();
+        _logger.debug("specific name is: " + specificName);
+
+        String sql = "select distinct GRANTEE,SCHEMA,'" + nodeName + "' as NAME ,SPECIFICNAME,GRANTOR,GRANTEETYPE,EXECUTEAUTH,GRANT_TIME from syscat.routineauth where schema = '" + schemaName + "' and specificname = '" + specificName + "'";
+        _logger.debug("sql to get privilege for procedure is: " + sql);
+        return new DataSet(null, sql, null, getNode().getSession().getInteractiveConnection());
+    }
+
+    public String getStatusMessage() {
+        return Messages.getString("DatabaseDetailView.Tab.Priviliges.status") + " " + getNode().getQualifiedName();
+    }
+    public String getSpecificName() throws Exception {
+    	String schemaName = getNode().getSchemaOrCatalogName();
+        String nodeName = getNode().toString();
+    	String sql = "select specificname from syscat.routines where routineschema = '" + schemaName + "' and routinename = '" + nodeName + "'";
+    	_logger.debug("sql to get specific name: " + sql);
+    	DataSet ds = new DataSet(null, sql, null, getNode().getSession().getInteractiveConnection());
+    	DataSetRow dsw[] = ds.getRows();
+    	_logger.debug("DataSetRow size during query specific name" + dsw.length);
+    	String specificName = (String)dsw[0].getObjectValue(0);
+    	return specificName;
+    }
+}
