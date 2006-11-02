551cbf2d20a55d9827b15080d94b873224df154c
wvernaillen <>
2006-11-02 19:07:41 +0000

    fixed link to screemshots

--- sqlexplorer_website/web/detail.php
+++ sqlexplorer_website/web/detail.php
@@ -1,10 +1,10 @@
 <?php include("common/header.php"); ?>
 
-<h1>Database Detail View</h1>
-<p>The database detail view shows detailed information for whatever node is selected in the database structure view.
-What is displayed will depend on the type of database that you are using.<br/>
-The detail information is cached for improved performance.  To refresh the information, make sure the table widget is active and press F5 to reload the information.</p>
-<p><img src="dbdetail1.jpg" /></p>
-<p><img src="dbdetail2.jpg" /></p>
+	<h1>Database Detail View</h1>
+	<p>The database detail view shows detailed information for whatever node is selected in the database structure view.
+	What is displayed will depend on the type of database that you are using.<br/>
+	The detail information is cached for improved performance.  To refresh the information, make sure the table widget is active and press F5 to reload the information.</p>
+	<p><img src="screenshots/dbdetail1.jpg" /></p>
+	<p><img src="screenshots/dbdetail2.jpg" /></p>
 
 <?php include("common/footer.php"); ?>
