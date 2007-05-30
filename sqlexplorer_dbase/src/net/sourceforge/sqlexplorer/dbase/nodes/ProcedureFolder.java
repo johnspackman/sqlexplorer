52aeb34b2c3696a0734942961e7dbc55c9f6967e
admorgan <>
2007-05-30 02:50:26 +0000

    Initial upload of DBase extension.  Only adds Procedure Folder to DBase listings.

---
+++ sqlexplorer_dbase/src/net/sourceforge/sqlexplorer/dbase/nodes/ProcedureFolder.java
@@ -0,0 +1,34 @@
+package net.sourceforge.sqlexplorer.dbase.nodes;
+
+import net.sourceforge.sqlexplorer.Messages;
+import net.sourceforge.sqlexplorer.dbstructure.nodes.AbstractSQLFolderNode;
+
+public class ProcedureFolder extends AbstractSQLFolderNode {
+
+	public ProcedureFolder() {
+	}
+
+	@Override
+	public String getChildType() {
+		return "procedure";
+	}
+
+	@Override
+	public String getName() {
+		return Messages.getString("dbase.dbstructure.procedures");
+	}
+
+	@Override
+	public String getSQL() {
+		return "SELECT so.Name, sc.text FROM "+
+			getSchemaOrCatalogName() +"..sysobjects so, "+ 
+			getSchemaOrCatalogName() +"..syscomments sc "+
+			"WHERE so.type='P' AND sc.id=so.id";
+	}
+
+	@Override
+	public Object[] getSQLParameters() {
+		return null;
+	}
+
+}
