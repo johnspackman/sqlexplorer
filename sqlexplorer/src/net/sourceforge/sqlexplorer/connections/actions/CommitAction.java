086c5214505eb8d86e99e4a0da1e45ef0d72261a
hhilbert <>
2010-03-15 22:13:36 +0000

    suppress log messages

--- sqlexplorer/src/net/sourceforge/sqlexplorer/connections/actions/CommitAction.java
+++ sqlexplorer/src/net/sourceforge/sqlexplorer/connections/actions/CommitAction.java
@@ -57,7 +57,7 @@ public class CommitAction extends AbstractConnectionTreeAction implements IViewA
 				if (!connection.getAutoCommit())
     				return true;
 			}catch(SQLException e) {
-				SQLExplorerPlugin.error("Cannot query auto commit state", e);
+//				SQLExplorerPlugin.error("Cannot query auto commit state", e);
 			}
 
         return false;
