816b5c48b811c204880ba8c4a65f2952b0c37317
hhilbert <>
2010-06-18 10:19:27 +0000

    fixed Index out of Bounds exception on calling Execute SQL-Chunk with statement ending at end of file

--- sqlexplorer/src/net/sourceforge/sqlexplorer/plugin/editors/SQLEditor.java
+++ sqlexplorer/src/net/sourceforge/sqlexplorer/plugin/editors/SQLEditor.java
@@ -957,7 +957,7 @@ public class SQLEditor extends EditorPart implements SwitchableSessionEditor {
 					{
 						lineNo++;
 					}
-					int endIndex = Math.min(text.getCharCount() - 1, text.getOffsetAtLine(lineNo)); 
+					int endIndex = lineNo < maxLines ? Math.min(text.getCharCount() - 1, text.getOffsetAtLine(lineNo)) : text.getCharCount() - 1; 
 					if(startIndex < endIndex)
 					{
 						sql = text.getText(startIndex, endIndex);
