44ab54d2a2980d4aee0ebe27e0332c7aa9eb13f8
hhilbert <>
2011-01-16 15:39:42 +0000

    added informix

--- sqlexplorer_website/web/resources.php
+++ sqlexplorer_website/web/resources.php
@@ -11,6 +11,7 @@
 		<li>CUBRID: <a href="http://www.cubrid.org/downloads">http://www.cubrid.org/downloads</a></li>
 		<li>HyperSQL Database Engine (HSQLDB): <a href="http://sourceforge.net/projects/hsqldb/files/">http://sourceforge.net/projects/hsqldb/files/</a></li>
 		<li>IBM Data Server (DB2): <a href="http://www-01.ibm.com/support/docview.wss?rs=4020&uid=swg21385217">http://www-01.ibm.com/support/docview.wss?rs=4020&uid=swg21385217</a></li>
+		<li>Informix: <a href="http://www14.software.ibm.com/webapp/download/search.jsp?go=y&rs=ifxjdbc">http://www14.software.ibm.com/webapp/download/search.jsp?go=y&rs=ifxjdbc</a></li>
 		<li>JayBird (Firebird): <a href="http://www.firebirdsql.org/index.php?op=files&id=jaybird">http://www.firebirdsql.org/index.php?op=files&id=jaybird</a></li>
 		<li>jTDS: <a href="http://sourceforge.net/projects/jtds/files/">http://sourceforge.net/projects/jtds/files/</a></li>
 		<li>Mckoi: <a href="http://www.mckoi.com/Download.html">http://www.mckoi.com/Download.html</a></li>
@@ -22,5 +23,6 @@
 		<li>SAP DB: <a href="http://www.sapdb.org/sap_db_jdbc.htm">http://www.sapdb.org/sap_db_jdbc.htm</a></li>
 		<li>SQLite: <a href="http://www.zentus.com/sqlitejdbc/">http://www.zentus.com/sqlitejdbc/</a></li>
 	</ul>
+	<p><emp>Last update: 2011/01/16</emp></p>
 					
 <?php include("common/footer.php"); ?>
