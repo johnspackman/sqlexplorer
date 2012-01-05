bf8909005b155d043de08d686bfca8488155ffdc
vladsrynty <>
2012-01-05 13:02:04 +0000

    - Removed unused imports

--- sqlexplorer_informix/src/net/sourceforge/sqlexplorer/informix/actions/explain/ExplainExecution.java
+++ sqlexplorer_informix/src/net/sourceforge/sqlexplorer/informix/actions/explain/ExplainExecution.java
@@ -8,7 +8,6 @@ import java.io.StringWriter;
 import java.sql.Blob;
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
