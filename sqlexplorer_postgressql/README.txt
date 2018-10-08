bf22e585bce369d5b7bd233fd0efdb56c05b9538
John Spackman <john.spackman@zenesis.com>
2018-10-08 10:31:24 +0100

    refactor to merge plugins and features into a single, simple plugin

--- sqlexplorer_postgressql/README.txt
+++
@@ -1,5 +0,0 @@
-This package is licensed under LGPL version 2.
-
-Some queries were taken from/inspired by pgadmin3 queries.
-
-Initial Author: Rocco Rutte <pdmef@gmx.net>
--- sqlexplorer_rcp/dist/dist.sh
+++
@@ -1,3 +0,0 @@
-cp -rvp updates/* /home/project-web/eclipsesql/htdocs/updates
-cp -rv dist/* /home/frs/project/e/ec/eclipsesql/
-touch "/home/frs/project/e/ec/eclipsesql/SQL Explorer Plugin"
